418348922309b1d71e65a092cb2e43591cbd242b
James Morgan <james.morgan@thebulb.africa>
2022-10-24 17:26:24 +0100

    added logic to group data by user and jobs

--- script.js
+++ script.js
@@ -9,12 +9,42 @@ createReadStream('./reactions.csv')
     const reaction = {
       user_id: row[0],
       job_id: row[1],
-      direction: row[2],
+      direction: row[2] === 'true',
       dateTime: row[3],
     };
+    // if (!reactions.length) reactions.push(reaction);
+    // else {
+    //   if (Number(reactions[reactions.length - 1].user_id) < +reaction.user_id)
+    //     reactions.push(reaction);
+    //   else reactions.unshift(reaction);
+    // }
     reactions.push(reaction);
   })
   .on('end', () => {
-    console.log(reactions.length);
+    reactions.sort((a, b) => +a.user_id - +b.user_id);
+    const userLikedJobs = reactions.reduce((acc, reaction) => {
+      const { user_id, job_id, direction } = reaction;
+      if (direction) {
+        const userData = {
+          user_id: user_id,
+          jobs: [job_id],
+        };
+        if (acc.length && acc[acc.length - 1].user_id === user_id) {
+          acc[acc.length - 1].jobs.push(job_id);
+        } else acc.push(userData);
+      }
+      return acc;
+    }, []);
+    // console.log(userLikedJobs.length);
+
+    const jobsLikedByUsers = reactions.reduce((acc, reaction) => {
+      const { user_id, job_id, direction } = reaction;
+      if (direction) {
+        if (acc[job_id]) acc[job_id].push(user_id);
+        else acc[job_id] = [user_id];
+      }
+      return acc;
+    }, {});
+    console.log(Object.keys(jobsLikedByUsers).length);
   })
   .on('error', error => console.error(error.message));
