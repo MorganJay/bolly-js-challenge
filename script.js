25530f9e0fe3245bc40a9ba1fbca38815bf105a0
James Morgan <james.morgan@thebulb.africa>
2022-10-25 05:10:32 +0100

    completed task 1

--- script.js
+++ script.js
@@ -6,38 +6,58 @@ const reactions = [];
 createReadStream('./reactions.csv')
   .pipe(parse({ delimiter: ',', from_line: 2 }))
   .on('data', row => {
-    const reaction = {
+    reactions.push({
       user_id: row[0],
       job_id: row[1],
       direction: row[2] === 'true',
       dateTime: row[3],
-    };
-    // if (!reactions.length) reactions.push(reaction);
-    // else {
-    //   if (Number(reactions[reactions.length - 1].user_id) < +reaction.user_id)
-    //     reactions.push(reaction);
-    //   else reactions.unshift(reaction);
-    // }
-    reactions.push(reaction);
+    });
   })
   .on('end', () => {
-    reactions.sort((a, b) => +a.job_id - +b.job_id);
-    const userLikedJobs = reactions.reduce((acc, reaction) => {
-      const { user_id, job_id, direction } = reaction;
-      if (direction) {
-        const userData = {
-          job_id,
-          users: [user_id],
-          likes : 1
-        };
-        if (acc.length && acc[acc.length - 1].job_id === job_id) {
-         if(!acc[acc.length - 1].users.includes(user_id)) acc[acc.length - 1].users.push(user_id);
-          acc[acc.length - 1].likes++;
-        } else acc.push(userData);
+    const userLikedJobs = reactions
+      .sort((a, b) => +a.user_id - +b.user_id)
+      .reduce((acc, reaction) => {
+        const { user_id, job_id, direction } = reaction;
+        if (direction) {
+          const userData = {
+            user_id: user_id,
+            jobs: [job_id],
+          };
+          if (acc.length && acc[acc.length - 1].user_id === user_id) {
+            acc[acc.length - 1].jobs.push(job_id);
+          } else acc.push(userData);
+        }
+        return acc;
+      }, []);
+
+    const similarityList = [];
+    for (let i = 0; i < userLikedJobs.length; i++) {
+      const { user_id, jobs } = userLikedJobs[i];
+      const userData = { user_id, jobs: [] };
+
+      for (let j = 1; j < userLikedJobs.length; j++) {
+        let count = 0;
+        jobs.forEach(job => {
+          if (userLikedJobs[j].jobs.includes(job)) {
+            count++;
+            if (userData.jobs.find(x => x.job === job))
+              userData.jobs.find(x => x.job === job).count++;
+            else userData.jobs.push({ job, count });
+
+            userData.jobs.sort((a, b) => b.count - a.count);
+          }
+        });
       }
-      return acc;
-    }, []);
-    console.log(userLikedJobs.sort((a , b) => b.likes - a.likes));
+      if (userData.jobs.length) similarityList.push(userData);
+    }
+
+    console.log(
+      similarityList.sort((a, b) => b.jobs[0].count - a.jobs[0].count)
+    );
+
+    console.log(
+      `The users with highest similarity are ${similarityList[0].user_id} and ${similarityList[1].user_id}`
+    );
 
     // const jobsLikedByUsers = reactions.reduce((acc, reaction) => {
     //   const { user_id, job_id, direction } = reaction;
